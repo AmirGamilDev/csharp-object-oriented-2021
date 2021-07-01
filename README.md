a3175ca281d22928caa85f6fd2db30459e425682
Amir Gamil <amir.gamil.dev@gmail.com>
2021-07-01 09:22:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# csharp-object-oriented-2021
+
+This repo was created to assist students on the Donegal ETB Software Developer course for the C# Object Oriented Programming module.
+
+Note 1: Some of the solutions do not always follow best practices.
+
+Note 2: You may need to change the connection string in the App.config file to get the solutions to work with your Database server.
---
+++ StateMaintenance/StateMaintenance/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StateMaintenance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void statesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Validate();
+                this.statesBindingSource.EndEdit();
+                this.tableAdapterManager.UpdateAll(this.mMABooksDataSet);
+            }
+            catch (DBConcurrencyException ex)
+            {
+                MessageBox.Show($"A concurrency error occurred: {ex.Message}", ex.GetType().ToString());
+                this.statesTableAdapter.Fill(this.mMABooksDataSet.States);
+            }
+            catch (DataException ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+                this.statesBindingSource.CancelEdit();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show($"Database error: {ex.Number}: {ex.Message}", ex.GetType().ToString());
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'mMABooksDataSet.States' table. You can move, or remove it, as needed.
+            try
+            {
+                this.statesTableAdapter.Fill(this.mMABooksDataSet.States);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show($"Database error: {ex.Number}: {ex.Message}", ex.GetType().ToString());
+            }
+        }
+
+        private void statesDataGridView_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            int row = e.RowIndex + 1;
+            string errorMessage = $"A data error occurred.\nRow: {row}\nError: {e.Exception.Message}";
+
+            MessageBox.Show(errorMessage, "Data error");
+        }
+    }
+}
