5812c419ed112375745791e42d78c3e0d1dda903
Amir Gamil <amir.gamil.dev@gmail.com>
2021-07-06 08:55:59 +0100

    Add ClassDemo

---
+++ ClassBasicsDemo/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassBasicsDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            Product cs15 = new Product("CS15", "Murach's C# 2015", 56.50M);
+            Product vb15 = new Product { Code = "VB15", Description = "Murach's Visual Basic 2015", Price = 56.50M };
+
+            string cs15DisplayText = cs15.GetDisplayText(",");
+            string vb15DisplayText = vb15.GetDisplayText("-");
+
+            MessageBox.Show(cs15DisplayText);
+            MessageBox.Show(vb15DisplayText);
+
+            string cs15ProductCode = cs15.Code;
+
+        }
+    }
+}
---
+++ ClassBasicsDemo/Product.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBasicsDemo
+{
+    class Product
+    {
+        private string code;
+        private string description;
+        private decimal price;
+
+        public string Code
+        {
+            get
+            {
+                return code;
+            }
+
+            set
+            {
+                code = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+
+            set
+            {
+                description = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+
+        public Product() { 
+        }
+
+        public Product(string code, string description, decimal price)
+        {
+            this.Code = code;
+            this.Description = description;
+            this.Price = price;
+        }
+
+        public string GetDisplayText(string sep)
+        {
+            return code + sep + price.ToString("c") + sep + description;
+        }
+    }
+}
