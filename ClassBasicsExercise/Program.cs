e9fd523c200ac5207d9cba039705b61effc44d91
Amir Gamil <amir.gamil.dev@gmail.com>
2021-07-07 15:25:31 +0100

    Add ClassBasicsExercise

---
+++ ClassBasicsExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBasicsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student bart = new Student(1234, "Bart", "Simpson", 'J');
+            Student lisa = new Student(5678, "Lisa", "Simpon", 'M');
+
+            string bartStudentInfo = bart.GetStudentInfo();
+            string lisaStudentInfo = lisa.GetStudentInfo();
+
+            Console.WriteLine(bartStudentInfo);
+            Console.WriteLine(lisaStudentInfo);
+
+            // using setter of LastName property
+            lisa.LastName = "Simpson";
+
+            // using getter of FullName property
+            Console.WriteLine("Lisa's full name: " + lisa.FullName);
+
+            Console.WriteLine(lisa.MiddleInitial);
+            lisa.MiddleInitial = 'S';
+            Console.WriteLine(lisa.MiddleInitial);
+        }
+    }
+}
---
+++ ClassBasicsExercise/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBasicsExercise
+{
+    class Student
+    {
+        private int studentId;
+        private string firstName;
+        private string lastName;
+        private char middleInitial;
+
+        public Student(int studentId, string firstName, string lastName, char middleInitial)
+        {
+            this.studentId = studentId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.MiddleInitial = middleInitial;
+        }
+
+        public int StudentId
+        {
+            get { return studentId; }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+
+            set { this.firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+
+            set { this.lastName = value; }
+        }
+
+        // example of auto-implemented property
+        public char MiddleInitial { get; set; }
+
+        public string FullName
+        {
+            get { return FirstName + " " + MiddleInitial + " " + LastName; }
+        }
+
+        public string GetStudentInfo()
+        {
+            return this.studentId + ", " + FullName;
+        }
+    }
+}
