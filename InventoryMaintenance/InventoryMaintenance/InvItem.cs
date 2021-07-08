24f6b733aca2f53562e71d6c42f479f485f34a2b
Amir Gamil <amir.gamil.dev@gmail.com>
2021-07-08 15:16:19 +0100

    Add InventoryMaintenance exercise

---
+++ InventoryMaintenance/InventoryMaintenance/InvItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMaintenance
+{
+    public class InvItem
+    {
+        public InvItem()
+        {
+        }
+
+        public InvItem(int itemNo, string description, decimal price)
+        {
+            this.ItemNo = itemNo;
+            this.Description = description;
+            this.Price = price;
+        }
+
+        public int ItemNo { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+        
+        public string GetDisplayText() => ItemNo + "    " + Description + " (" + Price.ToString("c") + ")";
+
+        // above method is equivalent to this:
+        //public string GetDisplayText()
+        //{
+        //    return ItemNo + "    " + Description + " (" + Price.ToString("c") + ")";
+        //}
+    }
+}
