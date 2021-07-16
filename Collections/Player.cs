c56d8ea3b041fc6a65b8fee729d5fc05e8372968
Amir Gamil <amir.gamil.dev@gmail.com>
2021-07-16 10:15:58 +0100

    Add solutions to parts 3A and 3B for collections exercise

---
+++ Collections/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public Player(string name, int score)
+        {
+            this.Name = name;
+            this.Score = score;
+        }
+    }
+}
--- Collections/Program.cs
+++ Collections/Program.cs
@@ -14,7 +14,81 @@ namespace Collections
 
             //runPart1B();
 
-            runPart2();
+            //runPart2();
+
+            //runPart3A();
+
+            runPart3B();
+        }
+
+        private static void runPart3B()
+        {
+            Queue<Player> players = new Queue<Player>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Please enter name for player {i + 1}:");
+                string name = Console.ReadLine();
+
+                Console.WriteLine($"Please enter score for player {i + 1}:");
+                int score = int.Parse(Console.ReadLine());
+
+                Player player = new Player(name, score);
+
+                players.Enqueue(player);
+            }
+
+            Console.WriteLine("\n\n");
+
+            double total = 0;
+            int count = players.Count;
+            int counter = 1;
+
+            while(players.Count > 0)
+            {
+                Player player = players.Dequeue();
+
+                total += player.Score;
+
+                Console.WriteLine($"Player {counter++}:\nPlayer Name: {player.Name}\nPlayer Score: {player.Score}\n");
+            }
+
+            double average = total / count;
+            Console.WriteLine($"Average Score: {average}");
+        }
+
+        private static void runPart3A()
+        {
+            Stack<Item> items = new Stack<Item>();
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Please enter name for item {i + 1}:");
+                string name = Console.ReadLine();
+
+                Console.WriteLine($"Please enter price for item {i + 1}:");
+                decimal price = decimal.Parse(Console.ReadLine());
+
+                Item item = new Item(name, price);
+
+                items.Push(item);
+            }
+
+            Console.WriteLine("\n\n");
+
+            while(items.Count > 0)
+            {
+                Item currentItem = items.Pop();
+
+                decimal vatPrice = CalculateItemVatPrice(currentItem);
+
+                Console.WriteLine($"Item: {currentItem.Name}\nPrice: {currentItem.Price}\nTotal Price: {vatPrice}");
+            }
+        }
+
+        private static decimal CalculateItemVatPrice(Item item)
+        {
+            return item.Price * 1.23M;
         }
 
         private static void runPart2()
