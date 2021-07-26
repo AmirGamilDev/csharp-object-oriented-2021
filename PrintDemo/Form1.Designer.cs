089ea9c968be10b89c3f7cc489457e4b29375d7c
Amir Gamil <amir.gamil.dev@gmail.com>
2021-07-26 13:59:13 +0100

    Add PrintDemo

---
+++ PrintDemo/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace PrintDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrintHelloWorld = new System.Windows.Forms.Button();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.SuspendLayout();
+            // 
+            // btnPrintHelloWorld
+            // 
+            this.btnPrintHelloWorld.Location = new System.Drawing.Point(71, 54);
+            this.btnPrintHelloWorld.Name = "btnPrintHelloWorld";
+            this.btnPrintHelloWorld.Size = new System.Drawing.Size(105, 23);
+            this.btnPrintHelloWorld.TabIndex = 0;
+            this.btnPrintHelloWorld.Text = "Print Hello World";
+            this.btnPrintHelloWorld.UseVisualStyleBackColor = true;
+            this.btnPrintHelloWorld.Click += new System.EventHandler(this.btnPrintHelloWorld_Click);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 151);
+            this.Controls.Add(this.btnPrintHelloWorld);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrintHelloWorld;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ PrintDemo/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrintDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrintHelloWorld_Click(object sender, EventArgs e)
+        {
+            printDialog1.Document = printDocument1;
+
+            if(printDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    printDocument1.Print();
+                }
+                catch(Win32Exception exception)
+                {
+                    MessageBox.Show(exception.Message, "Error");
+                }
+            }
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Font printFont = new Font("Arial", 16, FontStyle.Bold);
+
+            e.Graphics.DrawString("Hello world!", printFont, Brushes.Black, e.MarginBounds, StringFormat.GenericTypographic);
+        }
+    }
+}
